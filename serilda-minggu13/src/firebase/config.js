a0364d07d43dd545839cbfa201e6c73dbc9a5b5a
serildawn <43594595+serildawn@users.noreply.github.com>
2020-05-01 15:29:25 +0700

    minggu 13

---
+++ serilda-minggu13/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAXV2SHdnVvwaXt8cqiq2ra0niYckvR6tE",
+  authDomain: "fir-login-bb787.firebaseapp.com",
+  databaseURL: "https://fir-login-bb787.firebaseio.com",
+  projectId: "fir-login-bb787",
+  storageBucket: "fir-login-bb787.appspot.com",
+  messagingSenderId: "848709139213",
+  appId: "1:848709139213:web:982c0097d515a074838e44",
+  measurementId: "G-YZKVH5ETKZ"
+};
+
+export default firebaseConfig; 
